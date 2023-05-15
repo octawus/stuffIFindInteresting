c2125cf9dbab9feeee8d389cf700fb558d84d648
Octawus <mihai_stanescu700@yahoo.es>
2023-05-15 12:45:58 +0200

    Finished the project
    Did several changes to the code.
    Added more description to the readme.

--- MathExpressionEvaluator/test.py
+++ MathExpressionEvaluator/test.py
@@ -1,4 +1,4 @@
-from evaluator import calc
+from forreal import calc
 
 def test ():
     cases = (
@@ -11,14 +11,24 @@ def test ():
             ("3 * 5", 15),
             ("-7 * -(6 / 3)", 14),
             ("-40 / 36 - 36 + -95 / -69 + 82 / 50 - 33", -67.09429951690822),
-            ("-13 - 100 - 78 / 38 * -85 / 27 / -19 - -45", -68.03431644125794))
-    
-    special = (("-13 - 100 - 78 / 38 * -85 / 27 / -19 - -45", -68.03431644125794),)
-    
-    for case in special:
+            ("-13 - 100 - 78 / 38 * -85 / 27 / -19 - -45", -68.34010464758387),
+            ("(8) + (-1 * 29 - (65)) / (-28 + -(((-(-16 + 65)))) + 74)",  7.010526315789473),
+            ("(37) / (-73 * -87 - -(4)) - (62 - ((((3 - 96)))) * 30)",  -2851.994177812746),
+            ("(-98) * (100 + -60 - (69)) + (94 - -(((-(22 - -45)))) * -96)",  9368),
+            ("(71) - (13 + -33 - -(65)) - (-95 - ((((-47 + -62)))) / 2)", 66.5),
+            ("-(-39) - (41 / -27 - (14)) * (50 - -(((-(96 * 91)))) * -3)", 407524.25925925927),
+            ("-(51) + (-68 / -50 + -(39)) / (-84 - (((-(99 * -47)))) * -27)", -51.00029980804002))
+
+    no_of_tests = 0
+    no_of_successful_tests = 0   
+    for case in cases:
+        my_solution = calc(case[0])
         try:
-            assert calc(case[0]) == case[1], f"Expression {case[0]} returned {calc(case[0])} instead of {case[1]}"
+            no_of_tests += 1
+            assert my_solution == case[1], f"FAILED!!!!!!!!!!: Expression {case[0]} returned {my_solution} instead of {case[1]}"
+            no_of_successful_tests += 1
         except AssertionError as e:
             print(e)
-
+    print("Final report: {} test evaluated, {} succeeded, {} failed".format(no_of_tests, no_of_successful_tests, no_of_tests - no_of_successful_tests))
+        
 test()
